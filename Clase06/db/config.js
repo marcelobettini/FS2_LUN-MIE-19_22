f137886b7af35ad8a9606f1f8f81a1728c625cad
unknown <marcelobettini@yahoo.com.ar>
2022-03-30 22:04:39 -0300

    Clase 6

---
+++ Clase06/db/config.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2")
+const util = require("util")
+
+const pool = mysql.createPool({
+    host: process.env.db_host,
+    database: process.env.db_name,
+    user: process.env.db_user,
+    password: process.env.db_pass,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+
+});
+pool.getConnection((err, conn) => {
+    err ? console.warn("No conectado", { "error": err.message }) : console.log("Conexión establecida...")
+    pool.releaseConnection(conn)
+})
+pool.query = util.promisify(pool.query)
+
+module.exports = pool
---
+++ Clase06/users/usersController.js
@@ -0,0 +1,43 @@
+const { getAllUsers, getUserById, addNewUser, deleteUserById, editUserById } = require("./usersModel")
+const notNumber = require("../utils/notNumber")
+const { hashPassword, checkPassword } = require("../utils/handlePassword")
+
+//get all users 
+const listAll = async(req, res, next) => {
+    const dbResponse = await getAllUsers()
+    if (dbResponse instanceof Error) return next(dbResponse);
+    dbResponse.length ? res.status(200).json(dbResponse) : next()
+};
+
+//get user by id
+const listOne = async(req, res, next) => {
+    if (notNumber(req.params.id, next)) return;
+    const dbResponse = await getUserById(+req.params.id)
+    if (dbResponse instanceof Error) return next(dbResponse)
+    dbResponse.length ? res.status(200).json(dbResponse) : next()
+
+};
+//add new user
+const addOne = async(req, res) => {
+    const password = await hashPassword(req.body.password)
+    const dbResponse = await addNewUser({...req.body, password })
+    dbResponse instanceof Error ? next(dbResponse) : res.status(201).json({ message: `User ${req.body.name} created!` })
+}
+
+//delete user by id
+const removeOne = async(req, res, next) => {
+    if (notNumber(req.params.id, next)) return;
+    const dbResponse = await deleteUserById(+req.params.id)
+    if (dbResponse instanceof Error) return next(dbResponse);
+    dbResponse.affectedRows ? res.status(204).end() : next()
+}
+
+//Edit user by id
+const editOne = async(req, res) => {
+    if (notNumber(req.params.id, next)) return;
+    const dbResponse = await editUserById(+req.params.id, req.body)
+    if (dbResponse instanceof Error) return next(dbResponse);
+    dbResponse.affectedRows ? res.status(200).json(req.body) : next()
+}
+
+module.exports = { listAll, listOne, addOne, removeOne, editOne }
