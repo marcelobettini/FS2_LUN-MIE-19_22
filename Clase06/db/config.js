9d72e4741b6ab93e1b99b4dd50512bc2f6b366a7
Tomás <agu99tomas@gmail.com>
2022-04-01 00:00:20 -0300

    refactor: format config.js
    Formatea el código  de config.js

--- Clase06/db/config.js
+++ Clase06/db/config.js
@@ -1,20 +1,23 @@
-const mysql = require("mysql2")
-const util = require("util")
+const mysql = require("mysql2");
+const util = require("util");
 
 const pool = mysql.createPool({
-    host: process.env.db_host,
-    database: process.env.db_name,
-    user: process.env.db_user,
-    password: process.env.db_pass,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-
+  host: process.env.db_host,
+  database: process.env.db_name,
+  user: process.env.db_user,
+  password: process.env.db_pass,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
-pool.getConnection((err, conn) => {
-    err ? console.warn("No conectado", { "error": err.message }) : console.log("Conexión establecida...")
-    pool.releaseConnection(conn)
-})
-pool.query = util.promisify(pool.query)
 
-module.exports = pool
+pool.getConnection((err, conn) => {
+  err
+    ? console.warn("No conectado", { error: err.message })
+    : console.log("Conexión establecida...");
+  pool.releaseConnection(conn);
+});
+
+pool.query = util.promisify(pool.query);
+
+module.exports = pool;
