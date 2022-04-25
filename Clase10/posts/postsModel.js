045a5d80e5adb257f7a06bd125587c6f1f00fc2e
unknown <marcelobettini@yahoo.com.ar>
2022-04-25 20:30:39 -0300

    recuperamos async/await en los modelos users y posts

--- Clase10/posts/postsModel.js
+++ Clase10/posts/postsModel.js
@@ -1,26 +1,26 @@
 const pool = require("../db/config");
 
-const addNewPost = (post) => {
+const addNewPost = async (post) => {
     const query = "INSERT INTO posts SET ?"
     try {
-        return pool.query(query, post)
+        return await pool.query(query, post)
     } catch (error) {
         error.message = error.code
     }
 }
-const getAllPosts = () => {
+const getAllPosts = async () => {
     const query = "SELECT * FROM posts"
     try {
-        return pool.query(query)
+        return await pool.query(query)
     } catch (error) {
         error.message = error.code
     }
 }
 
-const getPostsWith = (string) => {
+const getPostsWith = async (string) => {
     const query = `SELECT * FROM posts WHERE title LIKE '%${string}%'`
     try {
-        return pool.query(query)
+        return await pool.query(query)
     } catch (error) {
         error.message = error.code
 
